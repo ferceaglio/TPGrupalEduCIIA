79429789fde82ba31bb86b5bdc43b9d9720da0cf
ferceaglio <ferceaglio@gmail.com>
2017-04-17 19:11:10 -0300

    se agregan archivos d Proyecto

---
+++ mefAscensor.c
@@ -0,0 +1,36 @@
+/*============================================================================
+ * Licencia: 
+ * Autor: 
+ * Fecha: 
+ *===========================================================================*/
+
+/*==================[inlcusiones]============================================*/
+
+#include "mefAscensor.h"   // <= su propio archivo de cabecera
+#include "sapi.h"       // <= Biblioteca sAPI
+
+/*==================[definiciones y macros]==================================*/
+
+/*==================[definiciones de datos internos]=========================*/
+
+/*==================[definiciones de datos externos]=========================*/
+
+/*==================[declaraciones de funciones internas]====================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+/*==================[definiciones de funciones internas]=====================*/
+
+/*==================[definiciones de funciones externas]=====================*/
+
+// Inicializar la MEF de ascensor
+void ascensorInicializarMEF( void ){
+   // TODO: hacer!!
+}
+
+// Actualizar la MEF de ascensor
+void ascensorActualizarMEF( void ){
+   // TODO: hacer!!
+}
+
+/*==================[fin del archivo]========================================*/
---
+++ programa.c
@@ -0,0 +1,48 @@
+/*============================================================================
+ * Licencia: 
+ * Autor: 
+ * Fecha: 
+ *===========================================================================*/
+
+/*==================[inlcusiones]============================================*/
+
+#include "programa.h"   // <= su propio archivo de cabecera
+#include "sapi.h"       // <= Biblioteca sAPI
+
+/*==================[definiciones y macros]==================================*/
+
+/*==================[definiciones de datos internos]=========================*/
+
+/*==================[definiciones de datos externos]=========================*/
+
+/*==================[declaraciones de funciones internas]====================*/
+
+/*==================[declaraciones de funciones externas]====================*/
+
+/*==================[funcion principal]======================================*/
+
+// FUNCION PRINCIPAL, PUNTO DE ENTRADA AL PROGRAMA LUEGO DE ENCENDIDO O RESET.
+int main( void ){
+
+   // ---------- CONFIGURACIONES ------------------------------
+   boardConfig();   // Inicializar y configurar la plataforma
+   
+   ascensorInicializarMEF();   // Inicializar la MEF de ascensor
+   
+   // ---------- REPETIR POR SIEMPRE --------------------------
+   while( TRUE )
+   {      
+      ascensorActualizarMEF();   // Actualizar la MEF de ascensor
+   } 
+
+   // NO DEBE LLEGAR NUNCA AQUI, debido a que a este programa se ejecuta 
+   // directamenteno sobre un microcontroladore y no es llamado/ por ningun
+   // Sistema Operativo, como en el caso de un programa para PC.
+   return 0;
+}
+
+/*==================[definiciones de funciones internas]=====================*/
+
+/*==================[definiciones de funciones externas]=====================*/
+
+/*==================[fin del archivo]========================================*/
