75f033a8cf7048c417461772879f4ed517eabf19
seba <sebastianilicic@gmail.com>
2017-04-25 23:53:46 -0300

    La verdad que ya a esta altura ni se que hice... no se que retornar de informacion al prog principal... ni tmp use delay antibloqueante... Use Uart para indicar el estado de la puerta o alarma pero no se si esta bien tmp... Trate pero la verdad es que no tuve mucho tiempo

--- app/src/mefPuertas.c
+++ app/src/mefPuertas.c
@@ -1,4 +1,40 @@
 #include "mefPuertas.h"
 #include "sapi.h"
+int main( void )
+{
+boardConfig();
+uartConfig(UART_USB, 115200);
+   int estado_puertas=1;//se abren
+    int a=1;
+        gpioWrite(LED3,estado_puertas);
+    uartWriteString(UART_USB, "Puerta abierta\n");
+    while (a==1)
+    {
+        /* int estado_puertas=1;//se abren
+         gpioWrite(LED3,estado_puertas);
+    uartWriteString(UART_USB, "Puerta abierta\n");
+    */delay(2000);
+       if(gpioRead(TEC1))
+        {
+            uartWriteString(UART_USB, "Puerta obstruida\n");
+            delay(2000);
+            while(gpioRead(TEC1))
+            {
+                uartWriteString(UART_USB, "Puerta obstruida\n");
+                uartWriteString(UART_USB, "Alarma de puerta obstruida\n");
+                gpioWrite(LEDB, ON);
+                delay(1000);
+                gpioWrite(LEDB,OFF);
+                delay(1000);
+            }
+        }    
+                estado_puertas=0;  //cierro puerta
+                gpioWrite(LED3,estado_puertas);
+                uartWriteString(UART_USB, "Puerta cerrada\n");
+            
+            
+        a=1;    
+    }
 
-// Codigo
+
+}
